bbfe1d5dcbd439db9607964397c43ffbac9bd833
yu-jongwon <40434632+yu-jongwon@users.noreply.github.com>
2022-04-14 22:13:05 +0900

    Update 3-9.c

--- 3-9.c
+++ 3-9.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
-    int n = 5;
+int main(int argc __attribute__((unused)), char** argv) {
+    int n = atoi(argv[1]);
     int a[n][n];
     
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            if (i == 0 || j == 0 || i == n-1 || j == n-1) {
-                if (j >= i) {
-                    a[i][j] = i+j+1;
-                } else {
-                    a[i][j] = (n-1)*4 - (i+j-1);
-                }
+            if (i == 0 || j == n-1) {
+                a[i][j] = i+j+1;
+            } else if (j == 0 || i == n-1) {
+                a[i][j] = (n-1)*4 - (i+j-1);
             } else {
                 a[i][j] = 0;
             }
