67bcb8ecb073e4179a56eb1f0baf8870418267f9
yu-jongwon <40434632+yu-jongwon@users.noreply.github.com>
2022-05-22 03:20:40 +0900

    Update poo.c

--- poo.c
+++ poo.c
@@ -7,33 +7,33 @@ int count = 0;
 char buffer[10] = {0};
 
 int factorial(int n) {
-	if (n == 1) return 1;
-	return n * factorial(n-1);
+  if (n == 1) return 1;
+  return n * factorial(n-1);
 }
 
 void combiantion(int index, char number) {
-	if (index == n) {
-		for (int k = 0; k < n; k++) {
-			printf("%c", buffer[k]);
-		}
-		if (++count < max) {
-			printf(", ");
-		}
-		return;
-	}
+  if (index == n) {
+    for (int k = 0; k < n; k++) {
+      printf("%c", buffer[k]);
+    }
+    if (++count < max) {
+      printf(", ");
+    }
+    return;
+  }
 
-	for (; number <= '9' - n + index + 1; number++) {
-		buffer[index] = number;
-		combiantion(index+1, number+1);
-	}
+  for (; number <= '9' - n + index + 1; number++) {
+    buffer[index] = number;
+    combiantion(index+1, number+1);
+  }
 }
 
 int main(int argc __attribute__((unused)), char* argv[]) {
-	n = atoi(argv[1]);
-	max = factorial(10) / (factorial(10 - n) * factorial(n));
+  n = atoi(argv[1]);
+  max = factorial(10) / (factorial(10 - n) * factorial(n));
 
-	combiantion(0, '0');
-	printf("\n");
+  combiantion(0, '0');
+  printf("\n");
 
-	return 0;
+  return 0;
 }
